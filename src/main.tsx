d93ee73317ed507925001087375ea2de81d11f88
v154254 <v154254@yandex.ru>
2023-12-20 16:20:59 +0500

    fix: actually add router to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import AppRouter from './router/AppRouter';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>
 );
