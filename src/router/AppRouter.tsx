dfa847559a6f97f96139740b8c238d49b53551f2
v154254 <v154254@yandex.ru>
2023-12-22 18:40:17 +0500

    feat: add product page to router

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -6,11 +6,24 @@ import {
 } from 'react-router-dom';
 
 import App from '../App';
+import ProductPage from '../pages/ProductPage';
+import { getProduct } from '../services/api';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<App />} />
+      <Route
+        path="products/:productID"
+        element={<ProductPage />}
+        loader={async ({ params }) => {
+          const { productID } = params;
+          if (productID) {
+            const response = await getProduct(+productID);
+            return response;
+          }
+        }}
+      />
     </>
   )
 );
