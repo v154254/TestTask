b6b29012550613976836e4c46c20a2811bd6ef87
v154254 <v154254@yandex.ru>
2023-12-24 01:40:44 +0500

    feat: add color selection to product page

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -2,14 +2,22 @@ import { useLoaderData } from 'react-router-dom';
 import ProductType from '../../types/types';
 import Slider from '../../components/Slider/Slider';
 import classes from './ProductPage.module.css';
+import { useState } from 'react';
 
 function ProductPage() {
   const product = useLoaderData() as ProductType;
+  const [currentColor, setCurrentColor] = useState(product.colors[0]);
   return (
     <div className={classes.container}>
       <h1>{product.name}</h1>
-      <Slider images={product.colors[0].images} />
-      <div>Colors</div>
+      <Slider images={currentColor.images} />
+      <div>
+        {product.colors?.map((color) => (
+          <button onClick={() => setCurrentColor(color)} key={color.id}>
+            {color.name}
+          </button>
+        ))}
+      </div>
       <div>Sizes</div>
     </div>
   );
