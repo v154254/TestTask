b1fc5549463f9c2bcbef5890e1b17b26e0020346
v154254 <v154254@yandex.ru>
2023-12-24 02:33:24 +0500

    feat: add loaging indicators to main page

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Product({ ...props }) {
+  const [loaded, setLoaded] = useState(false);
   return (
     <Link to={`products/${props.id}`}>
+      {loaded ? null : <p>Идёт загрузка, подождите...</p>}
       <img
+        style={loaded ? {} : { display: 'none' }}
+        onLoad={() => setLoaded(true)}
         src={props.image}
         alt="product image"
-        decoding="sync"
         width="300px"
         height="400px"
       />
