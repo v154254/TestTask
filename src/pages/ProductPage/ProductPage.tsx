b4c530dc68e0f14295f01e782d2e49c13fbad6c8
v154254 <v154254@yandex.ru>
2023-12-27 04:04:06 +0500

    refactor: all color info is saved in one state now

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,5 +1,5 @@
 import { useLoaderData } from 'react-router-dom';
-import ProductType from '../../types/ProductType';
+import ProductType, { ProductColor } from '../../types/ProductType';
 import Slider from '../../components/Slider/Slider';
 import classes from './ProductPage.module.css';
 import { useEffect, useState } from 'react';
@@ -13,8 +13,7 @@ function ProductPage() {
   const product = useLoaderData() as ProductType;
 
   const [firstRender, setFirstRender] = useState(true);
-  const [colorImages, setColorImages] = useState(['no image']);
-  const [availableSizes, setAvailableSizes] = useState([0]);
+  const [colorInfo, setColorInfo] = useState<ProductColor>(product.colors[0]);
 
   const { productID, colorID } = useAppSelector(
     (state) => state.SelectedProductSlice
@@ -35,8 +34,7 @@ function ProductPage() {
   useEffect(() => {
     getProductColor(productID, colorID).then(
       (color) => {
-        setColorImages(color.images);
-        setAvailableSizes(color.sizes);
+        setColorInfo(color);
       },
       (error) => {
         console.error(error);
@@ -47,9 +45,9 @@ function ProductPage() {
   return (
     <div className={classes.container}>
       <h1>{product.name}</h1>
-      <Slider images={colorImages} />
+      <Slider images={colorInfo?.images} />
       <ColorSelector availableColors={product.colors} />
-      <SizeSelector availableSizes={availableSizes} />
+      <SizeSelector availableSizes={colorInfo?.sizes} />
     </div>
   );
 }
