f5f486cfc478e14699324390b36da7eb7646bf18
v154254 <v154254@yandex.ru>
2023-12-28 01:14:37 +0500

    refactor: move images to project folder

--- src/services/api.ts
+++ src/services/api.ts
@@ -17,10 +17,7 @@ const products = [
       {
         id: 1,
         name: 'черный',
-        images: [
-          'https://raw.githubusercontent.com/ITCase/react-test-task/middle/public/images/1/black_front.png',
-          'https://raw.githubusercontent.com/ITCase/react-test-task/middle/public/images/1/black_back.png',
-        ],
+        images: ['/images/1/black_front.png', '/images/1/black_back.png'],
         price: '123.00',
         description: 'Описание для "Футболка черный"',
         sizes: [1, 2, 3],
@@ -28,10 +25,7 @@ const products = [
       {
         id: 2,
         name: 'белый',
-        images: [
-          'https://raw.githubusercontent.com/ITCase/react-test-task/middle/public/images/1/white_front.png',
-          'https://raw.githubusercontent.com/ITCase/react-test-task/middle/public/images/1/white_back.png',
-        ],
+        images: ['/images/1/white_front.png', '/images/1/white_back.png'],
         price: '125.00',
         description: 'Описание для "Футболка белый"',
         sizes: [1, 2, 3, 4, 5],
@@ -39,10 +33,7 @@ const products = [
       {
         id: 3,
         name: 'серый',
-        images: [
-          'https://raw.githubusercontent.com/ITCase/react-test-task/middle/public/images/1/gray_front.png',
-          'https://raw.githubusercontent.com/ITCase/react-test-task/middle/public/images/1/gray_back.png',
-        ],
+        images: ['/images/1/gray_front.png', '/images/1/gray_back.png'],
         price: '120.00',
         description: 'Описание для "Футболка серый"',
         sizes: [],
@@ -57,10 +48,7 @@ const products = [
       {
         id: 1,
         name: 'желтый',
-        images: [
-          'https://raw.githubusercontent.com/ITCase/react-test-task/middle/public/images/2/yellow_front.png',
-          'https://raw.githubusercontent.com/ITCase/react-test-task/middle/public/images/2/yellow_back.png',
-        ],
+        images: ['/images/2/yellow_front.png', '/images/2/yellow_back.png'],
         price: '88.00',
         description: 'Описание для "Майка желтый"',
         sizes: [1, 2, 3, 4, 5],
@@ -68,10 +56,7 @@ const products = [
       {
         id: 2,
         name: 'синий',
-        images: [
-          'https://raw.githubusercontent.com/ITCase/react-test-task/middle/public/images/2/blue_front.png',
-          'https://raw.githubusercontent.com/ITCase/react-test-task/middle/public/images/2/blue_back.png',
-        ],
+        images: ['/images/2/blue_front.png', '/images/2/blue_back.png'],
         price: '89.00',
         description: 'Описание для "Майка синий"',
         sizes: [2],
@@ -79,10 +64,7 @@ const products = [
       {
         id: 3,
         name: 'черный',
-        images: [
-          'https://raw.githubusercontent.com/ITCase/react-test-task/middle/public/images/2/black_front.png',
-          'https://raw.githubusercontent.com/ITCase/react-test-task/middle/public/images/2/black_back.png',
-        ],
+        images: ['/images/2/black_front.png', '/images/2/black_back.png'],
         price: '90.00',
         description: 'Описание для "Майка черный"',
         sizes: [],
