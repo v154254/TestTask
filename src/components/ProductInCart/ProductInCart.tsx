2cb9012b4fd923e263b3271fcea114ebf023edea
v154254 <v154254@yandex.ru>
2023-12-29 03:19:21 +0500

    feat: implement deleting items from cart

--- src/components/ProductInCart/ProductInCart.tsx
+++ src/components/ProductInCart/ProductInCart.tsx
@@ -2,11 +2,25 @@ import { useState } from 'react';
 import { getProduct, getProductColor, getSize } from '../../services/api';
 import ProductType, { ProductColor } from '../../types/ProductType';
 import SizeType from '../../types/SizeType';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import searchProductInCart from '../../utils/searchProductInCart';
+import { ShoppingCartSlice } from '../../store/reducers/ShoppingCartSlice';
 
 function ProductInCart({ ...props }) {
   const [productInfo, setProductInfo] = useState<ProductType>();
   const [colorInfo, setColorInfo] = useState<ProductColor>();
   const [sizeInfo, setSizeInfo] = useState<SizeType>();
+
+  const productsInCart = useAppSelector((state) => state.persistedReducer.cart);
+  const { removeProduct } = ShoppingCartSlice.actions;
+  const dispatch = useAppDispatch();
+
+  const product = {
+    productID: props.productID,
+    colorID: props.colorID,
+    sizeID: props.sizeID,
+  };
+  const index = searchProductInCart(productsInCart, product);
 
   getProduct(props.productID).then(
     (product) => {
@@ -44,7 +58,13 @@ function ProductInCart({ ...props }) {
       <p>Цвет: {colorInfo?.name}</p>
       <p>Стоимость: {colorInfo?.price}</p>
       <p>Размер: {sizeInfo?.label}</p>
-      <button>Удалить</button>
+      <button
+        onClick={() => {
+          dispatch(removeProduct(index));
+        }}
+      >
+        Удалить
+      </button>
     </div>
   );
 }
