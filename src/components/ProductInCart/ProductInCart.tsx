cac86077eae514fc73338f4983a6b271f463a7a5
v154254 <v154254@yandex.ru>
2023-12-28 05:36:24 +0500

    feat: now products properly displayed in cart

--- src/components/ProductInCart/ProductInCart.tsx
+++ src/components/ProductInCart/ProductInCart.tsx
@@ -1,9 +1,50 @@
+import { useState } from 'react';
+import { getProduct, getProductColor, getSize } from '../../services/api';
+import ProductType, { ProductColor } from '../../types/ProductType';
+import SizeType from '../../types/SizeType';
+
 function ProductInCart({ ...props }) {
+  const [productInfo, setProductInfo] = useState<ProductType>();
+  const [colorInfo, setColorInfo] = useState<ProductColor>();
+  const [sizeInfo, setSizeInfo] = useState<SizeType>();
+
+  getProduct(props.productID).then(
+    (product) => {
+      setProductInfo(product);
+    },
+    (error) => {
+      console.error(error);
+    }
+  );
+  getProductColor(props.productID, props.colorID).then(
+    (color) => {
+      setColorInfo(color);
+    },
+    (error) => {
+      console.error(error);
+    }
+  );
+  getSize(props.sizeID).then(
+    (size) => {
+      setSizeInfo(size);
+    },
+    (error) => {
+      console.error(error);
+    }
+  );
   return (
     <div>
-      {props.productID}
-      {props.colorID}
-      {props.sizeID}
+      <img
+        src={colorInfo?.images[0]}
+        alt="product image"
+        width="300px"
+        height="400px"
+      />
+      <h2>{productInfo?.name}</h2>
+      <p>Цвет: {colorInfo?.name}</p>
+      <p>Стоимость: {colorInfo?.price}</p>
+      <p>Размер: {sizeInfo?.label}</p>
+      <button>Удалить</button>
     </div>
   );
 }
--- src/pages/ShoppingCart/ShoppingCart.tsx
+++ src/pages/ShoppingCart/ShoppingCart.tsx
@@ -1,10 +1,12 @@
 import ProductInCart from '../../components/ProductInCart/ProductInCart';
 import { useAppSelector } from '../../hooks/redux';
+import classes from './ShoppingCart.module.css';
 
 function ShoppingCart() {
   const productsInCart = useAppSelector((state) => state.persistedReducer.cart);
+
   return (
-    <div>
+    <div className={classes.container}>
       {productsInCart?.map((product) => (
         <ProductInCart
           key={`${product.productID}${product.colorID}${product.sizeID}`}
